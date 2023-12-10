e6fbf85e722de9da2a45c7088f4771405d44111f
Boris_Razor <boria2008@inbox.ru>
2023-12-10 23:19:39 +0300

    Made changes to the solution of the first task

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -6,14 +6,14 @@
 // example:
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 5, 6, 7, 8"
-int NatureNumber(int start, int end)
+void NatureNumber(int start, int end)
 {
     if (start == end)
     {
         Console.Write($"{start}");
-        return start;
+        return;
     }
     Console.Write($"{start},");
-    return NatureNumber(start + 1, end);
+    NatureNumber(start + 1, end);
 }
 NatureNumber(4, 15);
