ea614bfd01671334c7651ecf58a8e3a51c605aef
Boris_Razor <boria2008@inbox.ru>
2023-12-12 11:23:22 +0300

    Added a solution to the second problem

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -4,4 +4,21 @@
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
 // example:
-// m = 2, n = 3 -> A(m,n) = 29
+// m = 3, n = 2 -> A(m,n) = 29
+
+int FunctionAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return (n + 1);
+    }
+    else if (n == 0)
+    {
+        return FunctionAkkerman(m-1,1);
+    }
+    else
+    {
+        return FunctionAkkerman(m-1,FunctionAkkerman(m,n-1));
+    }
+}
+Console.WriteLine(FunctionAkkerman(2,3));
